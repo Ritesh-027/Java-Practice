752d5f28462c06135c3b0544186fe45e1f34103c
Ritesh <riteshgaikwad309@gmail.com>
2022-07-20 17:39:35 +0530

    First Commit

--- Loops.java
+++ Loops.java
@@ -1,8 +1,19 @@
+import java.util.*;
+
 public class Loops {
 public static void main(String[] args){
+    //First pattern code by myself.
+    // Printing rectangle pattern depend on user inputs.
 
-    for(int i=10;i>=0;i--){
-            System.out.print("*");
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt(); 
+    int b = sc.nextInt(); 
+
+    for(int i=1;i<=a;i++){
+            for(int j=1;j<=b;j++){
+                System.out.print("*");
+                }
+            System.out.println();
         }
     } 
 }
