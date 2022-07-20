4c861cffaf9ac4a2776f2729a54080de33f500f1
Ritesh <riteshgaikwad309@gmail.com>
2022-07-20 15:21:03 +0530

    Some Changes

--- Print.java
+++ Print.java
@@ -1,8 +1,9 @@
 class Print {
 public static void main(String args[]){
- 
+
+    // These are types of printing statement in a different ways.
     System.out.println("Hey Ritesh");
-    System.out.print("Hey Ritesh\n");
+    System.out.print("Hey Ritesh/n");
     System.out.print("Hey Ritesh");
 }
 
