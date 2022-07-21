a767cab3f6ef9a3d315520e2817f51831abbc65e
Ritesh <riteshgaikwad309@gmail.com>
2022-07-21 17:44:04 +0530

    First Commit

--- Patterns_Practice.java
+++ Patterns_Practice.java
@@ -61,10 +61,53 @@ public static void main(String[] args){
                 System.out.println();
             }
     
-            System.out.println("180 Degree Inverted Half Pyramid !");
+            System.out.println("Half Pyramid with Numbers !");
     
+            for(int i=1; i<=5;i++){
+                for(int j=1;j<=i;j++){
+                    System.out.print(j+" ");
+                }
+                System.out.println();
+            }
             
+            System.out.println("Inverted Half Pyramid with Numbers !");
+
+            for(int i=1; i<=6;i++){
+                for(int j=1; j<=6-i; j++){
+                    System.out.print(j);
+                   }
+                System.out.println();
+            }
+
+            System.out.println("Floyd's Triangle !");
             
+            int num = 1;
+            for(int i=1; i<=5; i++){
+                for(int j=1; j<=i;j++){
+                    System.out.print(num+" ");
+                    num++;
+                }
+                
+                System.out.println();
+            }
+
+            System.out.println("0-1 Triangle !");
+
+            for(int i=1;i<=5;i++){
+                for(int j=1;j<=i;j++){
+                    int sum = i+j;
+                    if(sum % 2 == 0){
+                        System.out.print("1 ");
+                    }else{
+                        System.out.print("0 ");
+                        }
+                    }
+                    System.out.println();
+            }
+
+
+
+
     }
 
 }
