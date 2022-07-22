3c1bd254624733d940fa9db35d4b3156ee0674eb
Ritesh <riteshgaikwad309@gmail.com>
2022-07-22 19:37:08 +0530

    Practice

--- ptn1.java
+++ ptn1.java
@@ -1,19 +1,29 @@
 public class ptn1{
 
     public static void main(String[] args){
-
         for(int i=1; i<=5; i++){
-            for(int j=5; j<=5; j--){
-                System.out.print(" ");
-            }
-            for(int k=1; k<=5;k++){
-                System.out.print("*");
-            }
-            for(int j=1;j<=5;j++){
-                System.out.print(" ");
-            }
-               System.out.println();
+            
+            for(int j=1; j<=5-i+1;j++){
+            
+                System.out.print(j);
+            
+                }
+            System.out.println();
         }
-        
-}
+  
+
+        int n=5;
+        int num=1;
+        for(int i=1; i<=n; i++){
+            
+            for(int j=1; j<=i; j++){
+            
+                System.out.print(num+" ");
+                num++;
+            }
+
+            System.out.println();
+        }
+
+    }
 }
