41d24143e1221ef12c50771d050580591fffb03c
Ritesh <riteshgaikwad309@gmail.com>
2022-07-26 19:08:47 +0530

    Practice

---
+++ Functions/CheckIfPrime.java
@@ -0,0 +1,31 @@
+package Functions;
+
+class CheckIfPrime {
+  
+    static boolean isPrime(int n)
+    {
+        //We cannot check the negetive number that's why this condition!
+        if (n <= 1)
+            return false;// Return and exit from the function and set to the initial stage.
+        
+            //Checking if the given number is prime or not!
+
+        for (int i = 2; i < n; i++) //loop for checking that n is divided by any of the number from 1 to n!
+            if (n % i == 0)
+                return false;
+  
+        return true;
+    }
+  
+    public static void main(String args[])
+    {
+        if (isPrime(11)) // Calling function and printing the result here!
+            System.out.println(" true");
+        else
+            System.out.println(" false");
+        if (isPrime(2))
+            System.out.println(" true");
+        else
+            System.out.println(" false");
+    }
+}
