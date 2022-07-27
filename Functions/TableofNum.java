1d8a6ac09b8241de82efa9ac0ef09f6cf6d8e92a
Ritesh <riteshgaikwad309@gmail.com>
2022-07-28 03:01:34 +0530

    Table of n input

---
+++ Functions/TableofNum.java
@@ -0,0 +1,18 @@
+package Functions;
+import java.util.*;
+public class TableofNum {
+    public static void main(String[] args){
+        System.out.println("Input number is :");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        table(num);
+    }
+    
+    public static void table(int n){
+        int sum = 0;
+        for(int i=1; i<=10; i++){
+            sum = i * n;
+            System.out.println(sum);
+        }
+    }
+}
