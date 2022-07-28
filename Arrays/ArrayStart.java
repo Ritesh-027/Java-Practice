7ab34f98d0c3cddf7b103211597b66372a9099ac
Ritesh <riteshgaikwad309@gmail.com>
2022-07-29 02:16:54 +0530

    Define ary way 1!

--- Arrays/ArrayStart.java
+++ Arrays/ArrayStart.java
@@ -1,10 +1,26 @@
 public class ArrayStart{
     public static void main(String args[]){
         //One -D creating a array!
-        int[] oneDarray = new int[5];
+//        int[] oneDarray = new int[5];
 
         //Two - D creating a array!
-        int[][] twoDarray = new int [2][3];
+//s        int[][] twoDarray = new int [2][3];
+
+        //Defining array way 1!
+        int[] oneDarray = new int[5];
+
+        oneDarray[0] = 10;
+        oneDarray[1] = 20;
+        oneDarray[2] = 30;
+        oneDarray[3] = 40;
+        oneDarray[4] = 50;
+
+    //System.out.println(oneDarray[3]);
+
+        //Accessing the array data with loop.
+        for(int i=0; i<oneDarray[0]; i++){
+            System.out.println(oneDarray[i]);
+        }
 
     }
 }
---
+++ Arrays/Ary2.java
@@ -0,0 +1,15 @@
+public class Ary2 {
+    public static void main(String args[]){
+    //Defining array way 2!
+    
+    String[] ary = {"Shefali","Ritesh","Jay","Suraj","Tejas","Didi","Mittal","Priya","Akshay"};
+
+    //int[] num = {786,785,784,783,782,781,780};
+
+    for(int j=0; j<=8 ; j++){
+        System.out.println(ary[j]);
+        System.out.println();
+        }
+    }
+    
+}
