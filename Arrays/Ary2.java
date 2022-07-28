7bd022cfda6bdb903811cca02eab1a69b684f080
Ritesh <riteshgaikwad309@gmail.com>
2022-07-29 02:18:21 +0530

    Define ary way 2!

--- Arrays/Ary2.java
+++ Arrays/Ary2.java
@@ -2,7 +2,7 @@ public class Ary2 {
     public static void main(String args[]){
     //Defining array way 2!
     
-    String[] ary = {"Shefali","Ritesh","Jay","Suraj","Tejas","Didi","Mittal","Priya","Akshay"};
+    String[] ary = {"Shefali","Ritesh","Jay","Suraj","Tejas","Didi","Mittal","Priya","Akshay","24-10-1999"};
 
     //int[] num = {786,785,784,783,782,781,780};
 
