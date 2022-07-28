19c7f75f9110558b8775a750f663c58d6bb2b755
Ritesh <riteshgaikwad309@gmail.com>
2022-07-29 01:34:38 +0530

    Array Practice

---
+++ Arrays/ArrayStart.java
@@ -0,0 +1,10 @@
+public class ArrayStart{
+    public static void main(String args[]){
+        //One -D creating a array!
+        int[] oneDarray = new int[5];
+
+        //Two - D creating a array!
+        int[][] twoDarray = new int [2][3];
+
+    }
+}
