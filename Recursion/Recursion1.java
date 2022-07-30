40002f9255120c209d87d3f53960b61e5516dead
Ritesh <riteshgaikwad309@gmail.com>
2022-07-30 18:57:05 +0530

    Recursion practice start!

---
+++ Recursion/Recursion1.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class Recursion1 {
+    public static void main(String args[]){
+        int n=1;
+        System.out.println("Print 10 to 1 with recursion!");
+        printNum(n);
+
+    }
+
+    public static void printNum(int n){
+        if(n == 11){
+            return;
+        }
+        System.out.println(n);
+        printNum(n+1);
+    }
+
+
+    
+}
