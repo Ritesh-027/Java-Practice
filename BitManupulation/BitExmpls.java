a6d5af76e3ae247727b401ff0e4cb50ce007c251
Ritesh <riteshgaikwad309@gmail.com>
2022-08-04 10:33:26 +0530

    Practice

---
+++ BitManupulation/BitExmpls.java
@@ -0,0 +1,21 @@
+package BitManupulation;
+
+public class BitExmpls {
+    public static void main(String args[]){
+
+        //Get the 3rd bit.
+        int n = 5; //0101 - > Binary conversion of five.
+        int pos = 3;
+        int bitMask = 1<<pos; //-> 1 left shift 2.
+
+        // if both operation result is 0 or 1 then printing the value accordingly.
+        if((bitMask & n) == 0){
+            System.out.println("Bit was zero");
+        }else{
+            System.out.println("Bit was one");
+        }
+
+
+    }
+    
+}
