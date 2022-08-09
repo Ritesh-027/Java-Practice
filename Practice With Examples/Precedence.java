be3176112fdba42e488b9dddcd06ca7f3b761ff0
Ritesh <riteshgaikwad309@gmail.com>
2022-08-09 18:28:01 +0530

    Precedence

---
+++ Practice With Examples/Precedence.java
@@ -0,0 +1,23 @@
+
+public class Precedence{
+    public static void main(String args[]){
+
+        // Precedence of Operators.[Bodmass rull will not apply here.] 
+        // It's working on Precedence & Associativity.
+        
+        //--> Associativity LEFT to RIGHT
+        int a = 6 * 5 - 50 / 2; // 30 - 25 = 5
+        
+        // --> Associativity LEFT to RIGHT
+        int b = 50 / 2 - 6 * 5; // 25 - 30 = -5 // Both operator has same precedency in java.
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+
+
+    }
+
+}
