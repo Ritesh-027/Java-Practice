6511492d651d111aa5c28759fa429a8a9dd902e9
Ritesh <riteshgaikwad309@gmail.com>
2022-08-10 13:32:57 +0530

    String Exmpls

--- Practice With Examples/String_Exmples.java
+++ Practice With Examples/String_Exmples.java
@@ -4,5 +4,19 @@ public class String_Exmples{
         // String converting into lowercase.
         String name = "RITESH";
         System.out.println(name.toLowerCase());
+
+        // String converting into uppercase.
+        String name1 = "ritesh";
+        System.out.println(name1.toUpperCase());
+ 
+        // Replcing a string letter.
+        String name2 = "Shefali is arrogant";
+        System.out.println(name2.replace("is", "is not"));
+ 
+
+        // Replcing a string character.
+        String name3 = "Ritesh loves shefali!";
+        System.out.println(name3.replace('R', 'H'));
+ 
     }
 }
