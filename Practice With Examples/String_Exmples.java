efb4989f6b308ad58374def49bee9dc5cb0cd697
Ritesh <riteshgaikwad309@gmail.com>
2022-08-10 13:26:13 +0530

    String Exmpls

---
+++ Practice With Examples/String_Exmples.java
@@ -0,0 +1,8 @@
+public class String_Exmples{
+    public static void main(String args[]){
+
+        // String converting into lowercase.
+        String name = "RITESH";
+        System.out.println(name.toLowerCase());
+    }
+}
