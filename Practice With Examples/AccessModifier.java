9888f3fc3f6402e8089b6516ad5b378a7c60912b
Ritesh <riteshgaikwad309@gmail.com>
2022-08-16 12:39:09 +0530

    Acc_Modifier

---
+++ Practice With Examples/AccessModifier.java
@@ -0,0 +1,7 @@
+public class AccessModifier {
+
+    // For Classes, You can use only public or default access modifier.
+    //For Attrtibutes,Methods and constructures, You can use all the modifiers.
+    
+    
+}
